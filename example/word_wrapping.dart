30ba3d32271b6925fc9fe123ef69956b789bdf6a
Pazh-IT <mhwmdyaghubkhani@gmail.com>
2022-08-05 16:42:06 +0430

    added word-wrapping example

---
+++ example/word_wrapping.dart
@@ -0,0 +1,17 @@
+import 'package:tabulate/tabulate.dart';
+
+void main() {
+  Table table = Table();
+
+  table.addRow([
+    "This paragraph contains a veryveryveryveryveryverylong word. The long word will "
+        "break and word wrap to the next line.",
+    "This paragraph \nhas embedded '\\n' \ncharacters and\n will break\n exactly "
+        "where\n you want it\n to\n break."
+  ]);
+
+  table[0][0].theme.setWidth(20);
+  table[0][1].theme.setWidth(20);
+
+  print(table);
+}
